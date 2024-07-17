2d7c4d2ba5b170673d8b6ee245d24bb35079e0c2
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2024-07-17 18:59:03 +0200

    add compression in styles desk

--- src/styles/StylesDesk.tsx
+++ src/styles/StylesDesk.tsx
@@ -1,19 +1,26 @@
 import { Button } from "@mui/material";
 import { TransformerViewProps } from "../TransformerViewProps";
-import { StylizeArticulation, StylizeOrnamentation } from "mpmify/lib/transformers";
+import { CompressArticulation, CompressOrnamentation, StylizeArticulation, StylizeOrnamentation } from "mpmify/lib/transformers";
 import { Ornament } from "../../../mpm-ts/lib";
 
 export const StylesDesk = ({ msm, mpm, setMSM, setMPM }: TransformerViewProps) => {
     const transform = () => {
         const stylizeOrnaments = new StylizeOrnamentation()
+        const compressOrnaments = new CompressOrnamentation()
+
         const stylizeArticulation = new StylizeArticulation()
+        const compressArticulation = new CompressArticulation()
 
         stylizeOrnaments.transform(msm, mpm)
+        compressOrnaments.transform(msm, mpm)
+
         stylizeArticulation.transform(msm, mpm)
+        compressArticulation.transform(msm, mpm)
 
         setMSM(msm.clone())
         setMPM(mpm.clone())
     }
+
     return (
         <div style={{ width: '80vw', overflow: 'scroll' }}>
             <div>
