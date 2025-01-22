e8ddeda5a57b1737e6d1f997c680971eab0a7ddd
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2025-01-22 13:40:28 +0100

    improve readability

--- src/tempo/Box.tsx
+++ src/tempo/Box.tsx
@@ -95,6 +95,24 @@ export const Box = (props: BoxProps) => {
     }
   }
 
+  const markerLine = (
+    <MarkerLine
+      start={start}
+      stretchX={stretchX}
+      upperY={upperY}
+      marker={marker}
+      markerHovered={markerHovered}
+      segment={segment}
+      splitMode={splitMode}
+      setMarkerHovered={setMarkerHovered}
+      onRemoveMark={onRemoveMark}
+      onMark={onMark}
+      onSelectMark={onSelectMark}
+      onPlay={onPlay}
+      setHovered={setHovered}
+    />
+  )
+
   return (
     <g>
       {splitTime && (
@@ -109,6 +127,8 @@ export const Box = (props: BoxProps) => {
           />
         </>
       )}
+
+      {splitMode && markerLine}
 
       <polygon
         className='box'
@@ -144,37 +164,7 @@ export const Box = (props: BoxProps) => {
           }
         }} />
 
-      <line
-        className='marker'
-        x1={start * stretchX}
-        x2={start * stretchX}
-        y1={0}
-        y2={upperY}
-        stroke={marker ? 'red' : 'black'}
-        strokeWidth={(markerHovered || marker) ? 3 : 1}
-        strokeOpacity={markerHovered ? 0.3 : 0.8}
-        strokeDasharray={segment.silent ? '1 1' : undefined}
-        onMouseOver={() => {
-          setMarkerHovered(true)
-        }}
-        onMouseOut={() => {
-          setMarkerHovered(false)
-        }}
-        onClick={(e) => {
-          if (e.altKey && e.shiftKey) {
-            onRemoveMark()
-            return
-          }
-
-          if (!marker) {
-            onMark()
-          }
-          else {
-            onSelectMark()
-            onPlay(segment.date.start)
-          }
-          setHovered(true)
-        }} />
+      {!splitMode && markerLine}
 
       {marker?.continuous && (
         <text
@@ -189,5 +179,58 @@ export const Box = (props: BoxProps) => {
       )}
     </g>
   )
+}
+
+type MarkerLineProps = {
+  start: number
+  stretchX: number
+  upperY: number
+  marker: Marker | undefined
+  markerHovered: boolean
+  segment: TempoSegment
+  splitMode: boolean
+  setMarkerHovered: (hovered: boolean) => void
+  onRemoveMark: () => void
+  onMark: () => void
+  onSelectMark: () => void
+  onPlay: (start: number, end?: number) => void
+  setHovered: (hovered: boolean) => void
+}
+
+const MarkerLine: React.FC<MarkerLineProps> = (props: MarkerLineProps) => {
+  const { start, stretchX, upperY, marker, markerHovered, segment, splitMode, setMarkerHovered, onRemoveMark, onMark, onSelectMark, onPlay, setHovered } = props
+
+  return <line
+    className='marker'
+    x1={start * stretchX}
+    x2={start * stretchX}
+    y1={0}
+    y2={upperY}
+    stroke={marker ? 'red' : 'black'}
+    strokeWidth={(markerHovered || marker) ? 3 : 1}
+    strokeOpacity={markerHovered ? 0.3 : 0.8}
+    strokeDasharray={segment.silent ? '1 1' : undefined}
+    onMouseOver={() => {
+      if (splitMode) return
+      setMarkerHovered(true)
+    }}
+    onMouseOut={() => {
+      setMarkerHovered(false)
+    }}
+    onClick={(e) => {
+      if (e.altKey && e.shiftKey) {
+        onRemoveMark()
+        return
+      }
+
+      if (!marker) {
+        onMark()
+      }
+      else {
+        onSelectMark()
+        onPlay(segment.date.start)
+      }
+      setHovered(true)
+    }} />
 }
 
