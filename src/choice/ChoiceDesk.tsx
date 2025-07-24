0353aad48e079a03d63c0c2d5433dff5c846667e
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2025-07-25 00:32:11 +0200

    improve choicedesk

--- src/choice/ChoiceDesk.tsx
+++ src/choice/ChoiceDesk.tsx
@@ -1,10 +1,43 @@
-import { Button, FormControl, MenuItem, Select, Stack } from "@mui/material"
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, FormControl, MenuItem, Select, SelectChangeEvent, Stack } from "@mui/material"
 import { ScopedTransformerViewProps } from "../TransformerViewProps"
 import { MsmNote } from "mpmify/lib/msm"
 import { MouseEvent, useState } from "react"
-import { MakeChoice, RangeChoice } from "mpmify"
+import { MakeChoice, NoteChoice, Preference, RangeChoice } from "mpmify"
 import { createPortal } from "react-dom"
 import { Ribbon } from "../Ribbon"
+import { usePhysicalZoom } from "../hooks/ZoomProvider"
+
+const colors = [
+    "#e6194B",
+    "#3cb44b",
+    "#ffe119",
+    "#4363d8",
+    "#f58231",
+    "#911eb4"
+]
+
+interface SelectSourceProps {
+    sources: Set<string>
+    value: string
+    onChange: (event: SelectChangeEvent<string>) => void
+}
+
+const SelectSource = ({ sources, value, onChange }: SelectSourceProps) => {
+    return (
+        <FormControl fullWidth>
+            <Select
+                value={value}
+                onChange={onChange}
+            >
+                {Array.from(sources).map(source => (
+                    <MenuItem key={`select_${source}`} value={source}>
+                        {source}
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+    )
+}
 
 interface ArticulatedNoteProps {
     notes: MsmNote[]
@@ -13,43 +46,56 @@ interface ArticulatedNoteProps {
     onClick: (e: MouseEvent<Element>, note: MsmNote) => void
 }
 
-const NoteChoice = ({ notes, stretchX, stretchY, onClick }: ArticulatedNoteProps) => {
+const ChoiceGroup = ({ notes, stretchX, stretchY, onClick }: ArticulatedNoteProps) => {
+    const [hovered, setHovered] = useState(false)
+
     if (!notes.length) return null
 
     const refOnset = notes[0]["midi.onset"]
     const refVel = notes[0]["midi.velocity"]
     const refPitch = notes[0]["midi.pitch"]
 
+    const variationScore = notes.reduce((acc, note) => {
+        const velDiff = Math.abs(note["midi.velocity"] - refVel)
+        const onsetDiff = Math.abs(note["midi.onset"] - refOnset)
+        return acc + (velDiff + onsetDiff * 1000)
+    }, 0)
+
     return (
-        <g>
+        <g
+            opacity={hovered ? 1 : (variationScore / (notes.length * 2) || 0.2)}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
             <line
                 x1={refOnset * stretchX}
                 x2={refOnset * stretchX}
-                y1={(127 - refPitch - 1) * stretchY}
-                y2={(127 - refPitch + 1) * stretchY}
-                stroke="black"
-                strokeWidth={1}
+                y1={(100 - refPitch - 2) * stretchY}
+                y2={(100 - refPitch + 2) * stretchY}
+                stroke={'black'}
+                strokeWidth={2}
+                strokeOpacity={1}
             />
 
             {notes.map((note, i) => {
                 const velocity = note["midi.velocity"]
                 const duration = note["midi.duration"]
                 const onset = note["midi.onset"]
-                const yOffset = (1 / notes.length) * i
+                const yOffset = i - (notes.length / 2) + 0.5
                 const source = note.source || 'unknown'
                 const velDiff = Math.abs(velocity - refVel)
                 const onsetDiff = Math.abs(onset - refOnset)
                 if (i > 0 && ((velDiff <= 0 && onsetDiff <= 0.02))) return null
 
-                const y = (127 - note["midi.pitch"] + yOffset) * stretchY
+                const y = (100 - note["midi.pitch"] + yOffset) * stretchY
 
                 return (
                     <>
                         <line
                             data-source={source}
-                            strokeWidth={(1 / notes.length) * stretchY - 0.2}
+                            strokeWidth={stretchY - 0.4}
                             strokeOpacity={(velocity + 40) / 127}
-                            stroke={i > 0 ? 'red' : 'black'}
+                            stroke={colors[i % colors.length]}
                             x1={onset * stretchX}
                             x2={(onset + duration) * stretchX}
                             y1={y}
@@ -61,7 +107,7 @@ const NoteChoice = ({ notes, stretchX, stretchY, onClick }: ArticulatedNoteProps
                         <text
                             x={onset * stretchX}
                             y={y}
-                            fontSize={5}
+                            fontSize={10}
                             fill="white"
                             dominantBaseline="middle"
                         >
@@ -74,70 +120,57 @@ const NoteChoice = ({ notes, stretchX, stretchY, onClick }: ArticulatedNoteProps
     )
 }
 
-export const ChoiceDesk = ({ msm, part, addTransformer, appBarRef }: ScopedTransformerViewProps<MakeChoice>) => {
-    const [currentChoice, setCurrentChoice] = useState<RangeChoice>()
-    const [defaultChoice, setDefaultChoice] = useState<string>()
+export const ChoiceDesk = ({ msm, addTransformer, appBarRef }: ScopedTransformerViewProps<MakeChoice>) => {
+    const [currentChoice, setCurrentChoice] = useState<RangeChoice | NoteChoice>()
+    const [prefer, setPrefer] = useState<Preference>()
+    const [insert, setInsert] = useState(false)
 
-    const insert = () => {
-        if (currentChoice) {
-            addTransformer(new MakeChoice({
-                scope: part,
-                ...currentChoice
-            }))
-            setCurrentChoice(undefined)
-        }
-        else if (defaultChoice) {
-            addTransformer(new MakeChoice({
-                scope: part,
-                prefer: defaultChoice
-            }))
-        }
-    }
-
-    const stretchX = 30
+    const stretchX = usePhysicalZoom()
     const stretchY = 10
 
     const articulatedNotes = []
     // group notes with the same xml:id
     const grouped = Object.groupBy(msm.allNotes, note => note['xml:id'])
     for (const [xmlId, notes] of Object.entries(grouped)) {
-        if (!notes) return
+        if (!notes || !notes.length) return
 
         articulatedNotes.push((
-            <NoteChoice
+            <ChoiceGroup
                 key={`group_${xmlId}`}
                 notes={notes}
                 stretchX={stretchX}
                 stretchY={stretchY}
-                onClick={(e, note) => {
-                    if (!note.source) return
-
-                    if (!currentChoice || !e.shiftKey) {
-                        const newChoice: RangeChoice = {
-                            from: note.date,
-                            to: note.date,
-                            prefer: note.source
+                onClick={(e) => {
+                    if (!e.shiftKey && !e.metaKey) {
+                        const newChoice: NoteChoice = {
+                            noteids: [notes[0]['xml:id']],
                         }
                         setCurrentChoice(newChoice)
-                        return
                     }
-
-                    if (!('from' in currentChoice && 'to' in currentChoice && 'prefer' in currentChoice)) {
-                        return
-                    }
-
-                    if (e.shiftKey) {
-                        if (!currentChoice) return
-                        if (currentChoice.prefer === note.source) {
-                            if (note.date < (currentChoice as RangeChoice).from) {
-                                currentChoice.from = note.date
-                            }
-                            else if (note.date > (currentChoice as RangeChoice).to) {
-                                currentChoice.to = note.date
-                            }
+                    else if (currentChoice && e.metaKey && 'noteids' in currentChoice) {
+                        const noteId = notes[0]['xml:id']
+                        if (currentChoice.noteids.includes(noteId)) {
+                            currentChoice.noteids = currentChoice.noteids.filter(id => id !== noteId)
                         }
-                        const updated = { ...currentChoice }
-                        setCurrentChoice(updated)
+                        else {
+                            currentChoice.noteids.push(noteId)
+                        }
+                        setCurrentChoice({ ...currentChoice })
+                    }
+                    else if (currentChoice && e.shiftKey) {
+                        if ('noteids' in currentChoice) {
+                            const existingNotes = msm.allNotes.filter(note => currentChoice.noteids.includes(note['xml:id']))
+                            const from = Math.min(...existingNotes.map(note => note.date))
+                            const to = notes[0].date
+                            setCurrentChoice({
+                                from,
+                                to
+                            })
+                        }
+                        else {
+                            currentChoice.to = notes[0].date
+                            setCurrentChoice({ ...currentChoice })
+                        }
                     }
                 }}
             />
@@ -146,61 +179,144 @@ export const ChoiceDesk = ({ msm, part, addTransformer, appBarRef }: ScopedTrans
 
     const sources = new Set(msm.allNotes.map(note => note.source || 'unknown'))
 
-
     return (
         <>
-            {currentChoice && (
-                <>
-                    {createPortal((
-                        <Ribbon title="Range Choice">
-                            <Button
-                                size='small'
-                                variant='outlined'
-                                onClick={insert}
-                            >
-                                Choose {currentChoice.prefer.slice(0, 8)} ({(currentChoice as RangeChoice).from}-{(currentChoice as RangeChoice).to})
-                            </Button>
-                        </Ribbon>
-                    ), appBarRef.current || document.body)}
-                </>
-            )}
-
-            {!currentChoice && (
-                <>
-                    {createPortal((
-                        <Ribbon title="Default Choice">
-                            <Stack direction='row' spacing={1} alignItems='center'>
-                                <FormControl size='small'>
-                                    <Select
-                                        value={defaultChoice || ''}
-                                        onChange={(e) => {
-                                            setDefaultChoice(e.target.value)
-                                        }}
-                                    >
-                                        {Array.from(sources).map((source, index) => (
-                                            <MenuItem key={`source_${index}`} value={source}>
-                                                {source.slice(0, 8)}
-                                            </MenuItem>
-                                        ))}
-                                    </Select>
-                                </FormControl>
-
-                                <Button variant='contained' size='small' onClick={insert}>
-                                    Insert Default
-                                </Button>
-                            </Stack>
-                        </Ribbon>
-                    ), appBarRef.current || document.body)}
-                </>
-            )
-            }
-
+            {createPortal((
+                <Ribbon title="Range Choice">
+                    <Button
+                        size='small'
+                        variant='outlined'
+                        onClick={() => setInsert(true)}
+                    >
+                        Make Choice ({
+                            currentChoice
+                                ? 'noteids' in currentChoice
+                                    ? currentChoice.noteids.length
+                                    : `${currentChoice.from}-${currentChoice.to}`
+                                : 'Default'
+                        })
+                    </Button>
+                    <Button
+                        size='small'
+                        variant='outlined'
+                        onClick={() => {
+                            setCurrentChoice(undefined)
+                        }}
+                        disabled={!currentChoice}
+                    >
+                        Clear Choice
+                    </Button>
+                </Ribbon>
+            ), appBarRef.current || document.body)}
 
             <div style={{ width: '80vw', overflow: 'scroll', position: 'relative' }}>
                 <svg width={10000} height={900}>
                     {articulatedNotes}
                 </svg>
             </div>
+
+            <Dialog
+                open={insert}
+                onClose={() => setInsert(false)}
+            >
+                <DialogTitle>Make Choice</DialogTitle>
+                <DialogContent>
+                    <Stack spacing={2}>
+                        Select Preferred Source:
+                        <SelectSource
+                            sources={sources}
+                            value={prefer
+                                ? 'prefer' in prefer
+                                    ? prefer.prefer
+                                    : prefer.velocity
+                                : 'unknown'
+                            }
+                            onChange={(e) => {
+                                setPrefer({
+                                    prefer: e.target.value
+                                })
+                            }}
+                        />
+                        <Divider>
+                            Or
+                        </Divider>
+
+                        Preferred Source for Loudness (Velocity):
+                        <SelectSource
+                            sources={sources}
+                            value={prefer
+                                ? 'prefer' in prefer
+                                    ? prefer.prefer
+                                    : prefer.velocity
+                                : 'unknown'
+                            }
+                            onChange={(e) => {
+                                if (prefer && 'prefer' in prefer) {
+                                    setPrefer({
+                                        velocity: e.target.value,
+                                        timing: prefer.prefer,
+                                    })
+                                }
+                                else {
+                                    setPrefer({
+                                        velocity: e.target.value,
+                                        timing: prefer?.timing || 'unknown',
+                                    })
+                                }
+                            }}
+                        />
+
+                        Preferred Source for Timing:
+                        <SelectSource
+                            sources={sources}
+                            value={prefer
+                                ? 'prefer' in prefer
+                                    ? prefer.prefer
+                                    : prefer.timing
+                                : 'unknown'
+                            }
+                            onChange={(e) => {
+                                if (prefer && 'prefer' in prefer) {
+                                    setPrefer({
+                                        timing: e.target.value,
+                                        velocity: prefer.prefer,
+                                    })
+                                }
+                                else {
+                                    setPrefer({
+                                        timing: e.target.value,
+                                        velocity: prefer?.velocity || 'unknown',
+                                    })
+                                }
+                            }}
+                        />
+                    </Stack>
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        onClick={() => setInsert(false)}
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => {
+                            if (currentChoice && prefer) {
+                                addTransformer(new MakeChoice({
+                                    ...currentChoice,
+                                    ...prefer
+                                }))
+                            }
+                            setInsert(false)
+                            setCurrentChoice(undefined)
+                            setPrefer(undefined)
+                        }}
+                    >
+                        Make Choice
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }
